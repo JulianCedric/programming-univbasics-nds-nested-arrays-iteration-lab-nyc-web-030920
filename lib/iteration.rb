d6b2ec5c2cb2378e82bc2defe0d98f7cd5a2ddc0
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 16:18:00 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def find_greater_pair(array)
   while row_index < array.count do 
     inner_results = []
     inner_results << array[row_index].max
-    outer_results << inner_results
+    outer_results << inner_results.join('')
     row_index += 1
   end
   outer_results
