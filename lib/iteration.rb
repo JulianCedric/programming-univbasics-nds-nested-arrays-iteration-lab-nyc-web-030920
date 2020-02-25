8db6c5cceee109f43c20699c33dc6821d869475d
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 15:58:48 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,18 +1,67 @@
-def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+array_1 = [
+  ['pepperoni', 'sausage'], 
+  ['green olives', 'green peppers'], 
+  ['onions', 'pineapple']
+]
+
+def join_ingredients(array)
+
+outer_results = []
+row_index = 0 
+while row_index < array.count do 
+  element_index = 0
+  inner_results = []
+  while array[row_index][element_index] && array[row_index][element_index + 1] do  
+    inner_results << "I love #{array[row_index][element_index]} and #{array[row_index][element_index + 1]} on my pizza"
+    element_index += 1 
+  end
+  row_index += 1 
+  outer_results << inner_results.join('')
 end
 
-def find_greater_pair(src)
+outer_results
+
+end
+
+# array_2 = 
+# [
+#   [-1, -900], 
+#   [10, 30], 
+#   [0, 0], 
+#   [14, 16 * -2.5], 
+#   [Math.sin(1), 19]
+# ]
+
+def find_greater_pair(array)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+
+outer_results = []
+row_index = 0 
+
+while row_index < array.count do 
+  
+  element_index = 0 
+  inner_results = []
+  
+  while array[row_index][element_index] && array[row_index][element_index + 1] do
+    if array[row_index][element_index] > array[row_index][element_index + 1]
+      array[row_index][element_index + 1] = array[row_index][element_index]
+      inner_results << array[row_index][element_index]
+    else
+      array[row_index][element_index] = array[row_index][element_index + 1]
+      inner_results << array[row_index][element_index]
+    end
+  end
+  outer_results << inner_results
+  row_index += 1 
 end
+
+outer_results
+
+end 
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
